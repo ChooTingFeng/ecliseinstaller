69233bec5542bcd5e17daa6ece088b2fd5bc7163
ChooTingFeng <38828806+ChooTingFeng@users.noreply.github.com>
2020-07-06 20:32:10 +0800

    New stuff

---
+++ exportROW.bat
@@ -0,0 +1,4 @@
+adb pull /storage/emulated/0/Android/data/com.ea.game.pvz2_row/files/No_Backup/pp.dat
+adb pull /storage/emulated/0/Android/data/com.ea.game.pvz2_row/files/No_Backup/activequests/
+
+pause
