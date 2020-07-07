7b1b23d34a17259637490a9a52b2be9f6f37951c
ChooTingFeng <38828806+ChooTingFeng@users.noreply.github.com>
2020-07-07 14:07:57 +0800

    Update exportROW.bat

--- exportROW.bat
+++ exportROW.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Eclise save file exporter
-adb pull /storage/emulated/0/Android/data/com.ea.game.pvz2_row/files/No_Backup/pp.dat
-adb pull /storage/emulated/0/Android/data/com.ea.game.pvz2_row/files/No_Backup/activequests/
+adb pull -p /storage/emulated/0/Android/data/com.ea.game.pvz2_row/files/No_Backup/pp.dat
+adb pull -p /storage/emulated/0/Android/data/com.ea.game.pvz2_row/files/No_Backup/activequests/
 echo Done!
 pause
